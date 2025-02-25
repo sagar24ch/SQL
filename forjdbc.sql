bc0078c1fd1f47fcba0d01a752301c8495b3b4cc
Sagar Gorai <151768071+sagar24ch@users.noreply.github.com>
2025-02-25 17:47:15 +0530

    Add files via upload

---
+++ forjdbc.sql
@@ -0,0 +1,16 @@
+show databases;
+use myjdbcdb;
+describe myaccounts;
+show tables;
+desc jlcstudents;
+desc myaccounts;
+select * from myaccounts;
+update myaccounts set bal=5000 where accno=102;
+select * from myaccounts;
+update myaccounts set bal=5000 where accno=102;
+use myjdbcdb;
+select * from mycustomers;
+use myjdbcdb;
+show tables;
+
+select * from mystudents;
